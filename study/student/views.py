c3fad32187607f767d54129ac9859f34a0cc9d7f
LizaDanilenko <140692218+LizaDanilenko@users.noreply.github.com>
2025-05-20 08:49:42 +0000

    tmp

--- study/student/views.py
+++ study/student/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from student.models import Group, Faculty, Student
 
 # Create your views here.
+def detail(request):
+    faculties = Faculty.object.all()
+    print(faculties)
+
+
+
+    return HttpResponse("You're looking at ...")
